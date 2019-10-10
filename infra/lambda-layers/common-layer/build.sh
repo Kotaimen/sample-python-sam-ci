a417cc5f1cef6defef50688b009070e2b38f28c9
kotaimen <kotaimen.c@gmail.com>
2019-10-10 23:11:33 +0800

    Update lambda layer makefile and add lambda layer pipeline

--- infra/lambda-layers/common-layer/build.sh
+++ infra/lambda-layers/common-layer/build.sh
@@ -8,9 +8,12 @@ mkdir -p /tmp/staging/python
 pip install -r /var/task/requirements.txt -t /tmp/staging/python
 
 # zipping the dir
-cd /tmp/staging/
-zip -r9 -FS /var/build/layer.zip .
+cd /tmp/staging
+zip -r9 -FS /tmp/build/common-layer.zip .
 
-# hack: generate description with layer zip checksum
-cd /var/build/
-echo "Description: Python3.7 common layer, checksum=$(cat requirements.md5)" > template-fragment.yaml
+# hack: generate description with requirement checksum
+cd /tmp/build/
+echo "Description: $(cat common-layer.md5)" > common-layer.yaml
+
+du -h common-layer.zip
+cat common-layer.yaml
