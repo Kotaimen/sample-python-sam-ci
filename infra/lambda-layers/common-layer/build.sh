0bd3322cdb991e10612189a4201d061267450ead
kotaimen <kotaimen.c@gmail.com>
2020-03-04 23:28:42 +0800

    - Added lambda layer SSM output parameter. - Make layer runtime configurable.

--- infra/lambda-layers/common-layer/build.sh
+++ infra/lambda-layers/common-layer/build.sh
@@ -13,7 +13,7 @@ zip -r9 -FS /tmp/build/common-layer.zip .
 
 # hack: generate description with requirement checksum
 cd /tmp/build/
-echo "Description: $(cat common-layer.md5)" > common-layer.yaml
+echo "Description: $(cat common-layer.md5)" | tee > common-layer.yaml
 
 du -h common-layer.zip
-cat common-layer.yaml
+
